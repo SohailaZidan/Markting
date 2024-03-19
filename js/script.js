8ea454d252ca6955014cd606f2b18ba057be013f
Sohaila zidan <sohailaziedan533@gmail.com>
2024-03-19 15:16:48 +0200

    all project done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+menuBtn.onclick = () =>{
+   menuBtn.classList.toggle('fa-times');
+   navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+   menuBtn.classList.remove('fa-times');
+   navbar.classList.remove('active');
+}
+
